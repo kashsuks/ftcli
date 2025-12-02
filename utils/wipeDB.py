1cc5c6bb0f8b171c1355b5a9947a180614a4a1ae
Kashyap Sukshavasi <ksukshavasi@gmail.com>
2025-12-02 11:58:56 -0500

    chore: removed trailing whitespace

--- utils/wipeDB.py
+++ utils/wipeDB.py
@@ -4,12 +4,9 @@ from config import DB_URL
 
 async def wipe():
     conn = await asyncpg.connect(DB_URL)
-    
     await conn.execute("DROP TABLE IF EXISTS join_requests CASCADE;")
     await conn.execute("DROP TABLE IF EXISTS team_members CASCADE;")
     await conn.execute("DROP TABLE IF EXISTS teams CASCADE;")
     await conn.close()
-    
     print("Database wiped")
-    
 asyncio.run(wipe())
